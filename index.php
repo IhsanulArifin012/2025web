22f97fb036ab1cfed54f90f89cc4b7ff42c4075a
IhsanulArifin012 <ihsanul.arifin890@gmail.com>
2025-03-10 13:07:25 +0800

    create index

--- index.php
+++ index.php
@@ -7,5 +7,22 @@
 </head>
 <body>
     <h1>DATA MAHASISWA</h1>
+    <br>
+    <table border="1" cellspacing="0" cellpadding= "5">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>NIM</th>
+                <th>Nama</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>1</th>
+                <th>E020323012</th>
+                <th>Muhammad Ihsanul Arifin</th>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
